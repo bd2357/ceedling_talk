31cdf393deefb37c3ac0e994e24b1d8ec713ceed
Bryce Deary <bryce@bd2357.org>
2021-11-29 23:30:25 -0800

    Changes to slide deck proposal

---
+++ hash_explore/src/hash1.h
@@ -0,0 +1,5 @@
+#ifndef HASH1_H
+#define HASH1_H
+
+
+#endif // HASH1_H
---
+++ hash_explore/test/test_hash1.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "hash1.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_hash1_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement hash1");
+}
+
+#endif // TEST
